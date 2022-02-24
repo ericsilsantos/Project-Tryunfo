fc5a627579d50e9ad75e2796811e28e03518cf74
Eric Santos <eric.silvasantos44@gmail.com>
2022-02-24 17:33:32 -0300

    Adicionado o quinto requisito

--- src/App.js
+++ src/App.js
@@ -6,26 +6,57 @@ class App extends React.Component {
   constructor() {
     super();
 
-    this.handleCard = this.handleCard.bind(this);
+    this.handleCardPreview = this.handleCardPreview.bind(this);
+    this.handleBottonDisabled = this.handleBottonDisabled.bind(this);
 
     this.state = {
       cardName: '',
       cardDescription: '',
-      cardAttr1: '',
-      cardAttr2: '',
-      cardAttr3: '',
+      cardAttr1: '0',
+      cardAttr2: '0',
+      cardAttr3: '0',
       cardImage: '',
       cardRare: 'nomal',
       cardTrunfo: false,
+      hasTrunfo: false,
+      isSaveButtonDisabled: true,
     };
   }
 
-  handleCard({ target }) {
+  handleCardPreview({ target }) {
     const { name } = target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
-    // console.log(target);
+
     this.setState({
       [name]: value,
+    }, () => this.handleBottonDisabled());
+  }
+
+  handleBottonDisabled() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+    } = this.state;
+
+    let desativado = false;
+    const totalMaximo = 210;
+    const min = 0;
+    const max = 90;
+    const atrr1 = parseInt(cardAttr1, 10);
+    const atrr2 = parseInt(cardAttr2, 10);
+    const atrr3 = parseInt(cardAttr3, 10);
+    if (atrr1 > max || atrr2 > max || atrr3 > max) desativado = true;
+    if (atrr1 < min || atrr2 < min || atrr3 < min) desativado = true;
+    if ((atrr1 + atrr2 + atrr3) > totalMaximo) desativado = true;
+    if (!cardName || !cardDescription || !cardRare || !cardImage) desativado = true;
+
+    this.setState({
+      isSaveButtonDisabled: desativado,
     });
   }
 
@@ -39,6 +70,7 @@ class App extends React.Component {
       cardImage,
       cardRare,
       cardTrunfo,
+      isSaveButtonDisabled,
     } = this.state;
     return (
       <div>
@@ -53,7 +85,8 @@ class App extends React.Component {
             cardImage={ cardImage }
             cardRare={ cardRare }
             cardTrunfo={ cardTrunfo }
-            onInputChange={ this.handleCard }
+            isSaveButtonDisabled={ isSaveButtonDisabled }
+            onInputChange={ this.handleCardPreview }
           />
           <Card
             cardName={ cardName }
